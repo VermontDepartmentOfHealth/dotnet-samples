bba8e5483c47d9a911ce963f236df77862680c51
Mitofsky <Kyle.Mitofsky@vermont.gov>
2020-07-11 11:06:22 -0400

    add sample excel reader WIP

---
+++ SampleExcelReader/SampleExcelReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using Ganss.Excel;
+using System;
+
+namespace SampleExcelReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clients = new ExcelMapper("Clients.xlsx").Fetch<Client>();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Client
+    {
+        [Column("Client ID")]
+        public int ClientID { get; set; }
+
+        [Column("First Name")]
+        public string FirstName { get; set; }
+
+        [Column("Last Name")]
+        public string LastName { get; set; }
+
+        [Column("Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Column("Status")]
+        public string Status { get; set; }
+    }
+}
